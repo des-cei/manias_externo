fc28f34bc186090e26b0a09aaba32796082458b8
andrew <ai.romo.moena@upm.es>
2025-11-04 12:51:07 +0100

    listo para recopilar datos

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -3,9 +3,51 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 
-import warnings
-from sklearn.exceptions import ConvergenceWarning
-warnings.filterwarnings("ignore", category=ConvergenceWarning)
+from multiprocessing import Process, Manager
+import numpy as np
+
+
+# ===========================
+#  PROCESAMIENTO EN PARALELO
+# ===========================
+class Parallel:
+	# Los procesos son modelados como y(x)
+	# Los resultados --y-- se guardan como atributo de la clase
+	def __init__(self, func2eval, n_processes=12):
+		self.func2eval  = func2eval
+		self.n_processes = n_processes
+		self.manager    = Manager()
+		self.y          = self.manager.dict()
+
+	def split_lista(self, lst, n):
+		k, m = divmod(len(lst), n)
+		return [lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)]
+
+
+	def worker(self,chunk,chunk_idx):
+			resultados_chunk = []
+			for x_i in chunk:
+				resultados_chunk.append( self.func2eval(x_i) )
+			self.y[chunk_idx] = resultados_chunk
+
+	def evaluateY(self, x):
+		x = self.split_lista(x, self.n_processes)
+		self.y = self.manager.dict()
+		
+		all_process = []
+		for chunk_idx,chunk in enumerate(x):
+			t = Process(target=self.worker,args=[chunk,chunk_idx])
+			t.start()
+			all_process.append(t)
+
+		for t in all_process:
+			t.join()
+		
+		y = dict(self.y)
+		all_y = []
+		for key in sorted(y.keys()):
+			all_y += y[key]
+		return all_y
 
 
 def contarEnergia(instance, n_dim):
