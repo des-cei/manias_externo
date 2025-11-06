1b7678d505b0205a71ac1c9f852e533d53d0a835
andrew <ai.romo.moena@upm.es>
2025-11-06 10:42:45 +0100

    recopilacion datos subrogado kfold

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -2,9 +2,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
 
 from multiprocessing import Process, Manager
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # ===========================
@@ -49,14 +52,25 @@ class Parallel:
 			all_y += y[key]
 		return all_y
 
-
+# ===========================
+#       ANALISIS
+# ===========================
 def contarEnergia(instance, n_dim):
 	e = [0 for _ in range(n_dim)]
 	for time,idx in instance:
 		e[idx] += 1
 	return e
 
+# ===========================
+#       PLOTING
+# ===========================
+def rasterplot(instance):
+	time,channel = zip(*instance)
+	plt.scatter(time,channel, s=1, color='black')
 
+# ===========================
+#      EVALUATION
+# ===========================
 
 def fastEval(d_train, d_test, t_train, t_test, model='linear'):
 	scaler = StandardScaler().fit(d_train)
@@ -74,3 +88,35 @@ def fastEval(d_train, d_test, t_train, t_test, model='linear'):
 
 
 	return acc_test,acc_train
+
+
+
+def fastEvalKfold(data, targets, k=5, model='linear'):
+	
+	all_acc_train = []
+	all_acc_test  = []
+	kf = StratifiedKFold(n_splits=k,shuffle=True)
+	for idx_train, idx_test in kf.split(data,targets):
+		# ESCALAR DATOS
+		scaler = StandardScaler()
+		scaler.fit( data[idx_train] )
+		d_train = scaler.transform( data[idx_train] )
+		d_test  = scaler.transform( data[idx_test] )
+
+		# FIT MODEL
+		if model=='linear'			: model = LogisticRegression(solver='saga',max_iter=150)
+		elif model=='random_forest' : model = RandomForestClassifier()
+		model.fit( d_train, targets[idx_train] )
+	
+		# PREDICT
+		pred = model.predict(d_train)
+		all_acc_train.append( accuracy_score( targets[idx_train], pred ) )
+		pred = model.predict(d_test)
+		all_acc_test.append( accuracy_score( targets[idx_test], pred ) )
+
+	acc		  = np.mean(all_acc_test).round(3)
+	acc_train = np.mean(all_acc_train).round(3)
+	std       = np.std(all_acc_test).round(3)
+	std_train = np.std(all_acc_train).round(3)
+
+	return acc, std, acc_train, std_train
